7b5447b1b2f3a789b625899272537e671c2f3d10
Martin <martinbruun1995@gmail.com>
2023-09-12 23:44:33 +0200

    Text animations

---
+++ app.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// const letters = "abcdefghijklmnopqrstuvwxyz"
+
+// let interval = null;
+
+// document.querySelector("span").onmouseover = event => {
+//   let iteration = 0;
+
+//   clearInterval(interval);
+
+//   interval = setInterval(() => {
+//     event.target.innerText = event.target.innerText
+//       .split("")
+//       .map((letter, index) => {
+//         if(index < iteration) {
+//           return event.target.dataset.value[index];
+//         }
+
+//         return letters[Math.floor(Math.random() * 26)]
+//       })
+//       .join("");
+
+//     if(iteration >= event.target.dataset.value.length){
+//       clearInterval(interval);
+//     }
+
+//     iteration += 1 / 3;
+//   }, 30);
+// }
+
+const appDiv = document.querySelector('.app');
+const mask = document.querySelector('.mouse__mask')
+let pElements;
+
+function makeElementToGrid() {
+  for (let i = 0; i < 399; i++) {
+    const newP = document.createElement('p');
+    newP.textContent = 0;
+    appDiv.appendChild(newP);
+  }
+  pElements = document.querySelectorAll('p');
+}
+makeElementToGrid();
+
+pElements.forEach((element) => {
+  element.addEventListener('mouseenter', (e) => {
+    e.target.textContent = 1;
+    e.target.nextSibling.textContent = 1;
+    e.target.previousSibling.textContent = 1;
+  });
+});
+
+pElements.forEach((element) => {
+  element.addEventListener('mouseleave', (e) => {
+    e.target.textContent = 0;
+    e.target.nextSibling.textContent = 0;
+    e.target.previousSibling.textContent = 0;
+  });
+});
+
+document.addEventListener('mousemove', (e) => {
+  mask.style.top = e.pageY - 2500 + "px"
+  mask.style.left = e.pageX - 2500  + "px"
+  mask.style.animation = 'initial'
+  
+});
+
+document.addEventListener('mouseout', () => {
+  mask.style.top = "calc(50% - 2500px)"
+  mask.style.left = "calc(50% - 2500px)"
+  mask.style.animation = 'maskSpin 10s infinite linear'
+})
+
